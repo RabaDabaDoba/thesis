3e3c5bcfbc14ee4ebe489a8901bfee8e0dbb3124
RabaDabaDobaj <robindah@kth.se>
2019-05-06 00:02:37 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //SimpleUI ui=new SimpleUI();
+        //ui.setVisible(true);
+        
+        String path = "C:/Users/Robin/Desktop/Skolarbete/Thesis/Data/mango/61TbBBpszTL._SX425_.jpg";
+        String test = RobinsVersion.classifyImage(path);
+        //JSONObject jo = (JSONObject) RobinsVersion.classifyImage(path); 
+        
+        
+        
+        
+    }
+    
+     
+
+}
